ae53479ee708310d131a45a075ed30a93a399091
naruepon sowchai <naruepon.sao@ku.th>
2022-02-25 16:00:55 +0700

    v1

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Route, Link, Routes } from 'react-router-dom'
+import UploadImg from './component/UploadImg'
+const Sketchfab = () => {
+  return (
+    <div style={{ height: '100%', width: '100%', alignItems: 'center', flex: 1 }}>
+      <div
+        style={{
+          height: 600,
+          width: 750,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          borderStyle: 'solid',
+          marginTop: '3%',
+          borderWidth: '10',
+          borderColor: '#343434'
+        }}>
+        <iframe
+          style={{ height: 600, width: 750 }}
+          title="cartoon"
+          frameborder="0"
+          allowfullscreen
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+          allow="autoplay; fullscreen; xr-spatial-tracking"
+          xr-spatial-tracking
+          execution-while-out-of-viewport
+          execution-while-not-rendered
+          web-share
+          src="https://sketchfab.com/models/c138707cd4bc45de8d7d05f2bcf98175/embed"></iframe>
+      </div>
+    </div>
+  )
+}
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <li>
+          <Link to="/Sketchfab">Sketchfab</Link>
+        </li>
+        <li>
+          <Link to="/Screen3drender">Screen3drender</Link>
+        </li>
+      </nav>
+      <Routes>
+        <Route path="/Sketchfab" element={<Sketchfab />}></Route>
+        <Route path="/Screen3drender" element={<UploadImg />}></Route>
+      </Routes>
+    </div>
+  )
+}
+export default App
---
+++ src/component/UploadImg.js
@@ -0,0 +1,74 @@
+import axios from 'axios'
+import React, { Suspense, useLayoutEffect, useState } from 'react'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { Stage, OrbitControls } from '@react-three/drei'
+import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader'
+function Model(props) {
+  const geo = useLoader(PLYLoader, props.part)
+
+  useLayoutEffect(() => void geo.computeVertexNormals(), [geo])
+  return (
+    <mesh castShadow receiveShadow geometry={geo} {...props}>
+      <meshStandardMaterial color="#e2872d" flatShading />
+    </mesh>
+  )
+}
+
+const UploadImg = () => {
+  const [file, setFile] = useState({})
+  const [imagePreviewUrl, setImagePreviewUrl] = useState(null)
+  const onClickUpload = async () => {
+    const formData = new FormData()
+    formData.append('file', file)
+    const uploadImg = await axios({
+      method: 'post',
+      url: 'http://127.0.0.1:5000/upload',
+      data: formData
+    })
+  }
+  const handleUploadImage = (e) => {
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      setFile(file)
+      setImagePreviewUrl(reader.result)
+    }
+    reader.readAsDataURL(file)
+  }
+  return (
+    <React.Fragment>
+      <div style={{ height: '100%', width: '100%', alignItems: 'center', flex: 1 }}>
+        <div
+          style={{
+            backgroundColor: '#a1a7a41a',
+            height: 600,
+            width: 750,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            borderStyle: 'solid',
+            marginTop: '5%',
+            borderWidth: '10',
+            borderColor: '#343434'
+          }}>
+          <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 0], fov: 55 }}>
+            <Suspense fallback={null}>
+              <Stage>
+                <Model scale={0.01} part={imagePreviewUrl ? imagePreviewUrl : '/T1.ply'} />
+              </Stage>
+            </Suspense>
+            <OrbitControls />
+          </Canvas>
+        </div>
+        <div style={{ marginTop: 20, textAlign: 'center' }}>
+          <h1>This is a ply file</h1>
+        </div>
+
+        <div style={{ marginTop: 50, marginBottom: 50, textAlign: 'center' }}>
+          <input type="file" onChange={handleUploadImage} />
+          <button onClick={onClickUpload}> Upload </button>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+export default UploadImg
